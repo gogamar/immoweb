cf4ee4745994e9795d7a18189b16905af6d0706e
Gordana Marjanovic <gogamar@gmail.com>
2024-05-13 23:25:31 +0200

    el

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
@@ -11,3 +10,4 @@ pin "slick-carousel", to: "https://ga.jspm.io/npm:slick-carousel@1.8.1/slick/sli
 pin "magnific-popup", to: "https://ga.jspm.io/npm:magnific-popup@1.1.0/dist/jquery.magnific-popup.js"
 pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@3.1.2/dist/mapbox-gl.js"
 pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
+pin "application", preload: true
