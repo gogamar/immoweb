46c29574f116775176bc1bc5a219b5b5db46995f
Gordana Marjanovic <gogamar@gmail.com>
2024-05-14 11:30:49 +0200

    remove stimulus components

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -13,19 +13,19 @@ eagerLoadControllersFrom("controllers", application);
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
-import {
-  Alert,
-  Dropdown,
-  Modal,
-  Tabs,
-  Popover,
-  Toggle,
-  Slideover,
-} from "tailwindcss-stimulus-components";
-application.register("alert", Alert);
-application.register("dropdown", Dropdown);
-application.register("modal", Modal);
-application.register("tabs", Tabs);
-application.register("popover", Popover);
-application.register("toggle", Toggle);
-application.register("slideover", Slideover);
+// import {
+//   Alert,
+//   Dropdown,
+//   Modal,
+//   Tabs,
+//   Popover,
+//   Toggle,
+//   Slideover,
+// } from "tailwindcss-stimulus-components";
+// application.register("alert", Alert);
+// application.register("dropdown", Dropdown);
+// application.register("modal", Modal);
+// application.register("tabs", Tabs);
+// application.register("popover", Popover);
+// application.register("toggle", Toggle);
+// application.register("slideover", Slideover);
