9d8bde69f2038d7619aee75cffac30a878b7003f
Gordana Marjanovic <gogamar@gmail.com>
2024-05-14 10:39:10 +0200

    test

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Import third-party libraries
 import "@hotwired/turbo-rails";
+import "jquery";
+import "slick-carousel";
+import "magnific-popup";
+import "mapbox-gl";
+
+// Import Tailwind CSS Stimulus Components (assuming they are JavaScript modules)
+import "tailwindcss-stimulus-components";
+
+// Import controllers (assuming they are Stimulus controllers)
 import "controllers";
 
-import "./add_jquery";
-import { initSlick } from "./components/init_slick";
-import { initMagnificPopUp } from "./components/init_magnificPopUp";
+// import "./add_jquery";
+// import { initSlick } from "./components/init_slick";
+// import { initMagnificPopUp } from "./components/init_magnificPopUp";
 
-document.addEventListener("turbo:load", function () {
-  initSlick();
-  initMagnificPopUp();
-});
+// document.addEventListener("turbo:load", function () {
+//   initSlick();
+//   initMagnificPopUp();
+// });
