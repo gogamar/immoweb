8f53aa72325c8014c54edd3007b9286545fa0e9c
Gordana Marjanovic <gogamar@gmail.com>
2024-05-14 10:28:18 +0200

    t

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,13 +1,12 @@
 # Pin npm packages by running ./bin/importmap
-
+pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "tailwindcss-stimulus-components" # @4.0.4
+
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
 pin "slick-carousel", to: "https://ga.jspm.io/npm:slick-carousel@1.8.1/slick/slick.js"
 pin "magnific-popup", to: "https://ga.jspm.io/npm:magnific-popup@1.1.0/dist/jquery.magnific-popup.js"
 pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@3.1.2/dist/mapbox-gl.js"
-pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
-pin "application", preload: true
+pin "tailwindcss-stimulus-components", to: "https://cdn.jsdelivr.net/npm/tailwindcss-stimulus-components@5.0.2/+esm", preload: true
