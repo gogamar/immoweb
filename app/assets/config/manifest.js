1b1dbc265687474e822b671cb702bc6803a7cf22
Gordana Marjanovic <gogamar@gmail.com>
2024-05-15 12:05:36 +0200

    test

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,3 +3,6 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+//= link add_jquery.js
+//= link init_slick.js
+//= link init_magnificPopUp.js
