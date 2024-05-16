f795e70af41f19b7b976e88d605a496aeb58152c
Gordana Marjanovic <gogamar@gmail.com>
2024-05-16 11:44:44 +0200

    dropdown

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -12,6 +12,3 @@ eagerLoadControllersFrom("controllers", application);
 
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
-
-import Dropdown from "@stimulus-components/dropdown";
-application.register("dropdown", Dropdown);
