be81b090bbbfdb570dfda443a7da4f6ad683175c
Gordana Marjanovic <gogamar@gmail.com>
2024-11-08 11:28:07 +0100

    sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,9 @@
 Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV['REDIS_URL'],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+
   config.on(:startup) do
     schedule_file = "config/schedule.yml"
 
